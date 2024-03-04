3b0d6bff43974b92918956e363956fad0cb89b2d
Philippamadill <philippamadill@gmail.com>
2024-03-04 16:45:09 -0700

    created all the tables

--- database.sql
+++ database.sql
@@ -1,16 +1,75 @@
 -- Create a new database
-CREATE DATABASE your_database_name;
+CREATE DATABASE INVENTORY;
 
 -- Use the newly created database
-USE your_database_name;
+USE INVENTORY;
 
 -- Create a table
-CREATE TABLE your_table_name (
-    column1 datatype,
-    column2 datatype,
-    column3 datatype,
-    ...
-);
+CREATE TABLE STOCKS (
+    p_id int,
+    s_name varchar(255));
+    -- Create a table
+CREATE TABLE MANAGERS (
+    Managers_ID int,
+    store varchar(255));
+    -- Create a table
+CREATE TABLE EMPLOYEE (
+    Username_ID varchar(255),
+    pw varchar(255),
+    full_Name varchar(255),
+    hired_by varchar(255),
+    fired_by varchar(255));
+    -- Create a table
+CREATE TABLE UPDATES_QUANTITY (
+	e_id varchar(255),
+    Product_ID int);
+    -- Create a table
+CREATE TABLE MANUFACTURER (
+    m_name varchar(255),
+    supplies_by varchar(255),
+    request_by varchar(255));
+    -- Create a table
+CREATE TABLE STORE (
+    s_name varchar(255),
+    location int);
+    -- Create a table
+CREATE TABLE LOCATION (
+    Location_ID int,
+    country varchar(255),
+    province varchar(255),
+    city varchar(255));
+    -- Create a table
+CREATE TABLE REQUEST (
+    managers_id int,
+    manu_id varchar(255));
+    -- Create a table
+CREATE TABLE PRODUCT (
+    product_id int,
+    price float,
+    quantity int,
+    Added_By varchar(255),
+    removed_by varchar(255),
+    supplier varchar(255));
+    -- Create a table
+CREATE TABLE BOOKS (
+    p_id int,
+    title varchar(255),
+    author varchar(255),
+    genre varchar(255));
+    -- Create a table
+CREATE TABLE CLOTHING (
+    p_id int,
+    size varchar(255),
+    colour varchar(255),
+    style varchar(255));
+    -- Create a table
+CREATE TABLE FOOD (
+    p_id int,
+    expiration varchar(255));
+    -- Create a table
+CREATE TABLE WORKS_AT (
+    e_id varchar(255),
+    s_name varchar(255));
 
 -- Insert data into the table
 INSERT INTO your_table_name (column1, column2, column3, ...)
